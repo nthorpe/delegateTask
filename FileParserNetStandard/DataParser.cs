d748b5a11072ab0efd587749d37b31d5c5a650e5
GitSiracusa <7258267@student.swin.edu.au>
2019-07-03 23:13:25 +1000

    init

---
+++ FileParserNetStandard/DataParser.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FileParserNetStandard {
+    public class DataParser {
+        
+
+        /// <summary>
+        /// Strips any whitespace before and after a data value.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public List<List<string>> StripWhiteSpace(List<List<string>> data) {
+            List<List<string>> newList = new List<List<string>>();
+
+            foreach (var row in data)
+            {
+                var newRow = new List<string>();
+                foreach (string item in row)
+                {
+                    newRow.Add(item.Trim());
+                }
+                newList.Add(newRow);
+            }
+            return newList;
+    }
+
+        /// <summary>
+        /// Strips quotes from beginning and end of each data value
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public List<List<string>> StripQuotes(List<List<string>> data) {
+
+            return data;
+        }
+
+    }
+}
---
+++ FileParserNetStandard/FileHandler.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace FileParserNetStandard {
+    public class FileHandler {
+       
+        public FileHandler() { }
+
+        /// <summary>
+        /// Reads a file returning each line in a list.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public List<string> ReadFile(string filePath) {
+            List<string> lines = new List<string>();
+
+            try
+            {   // Open the text file using a stream reader.
+                using (var sr = new StreamReader(filePath))
+
+                    while (!sr.EndOfStream)
+                    {
+                        lines.Add(sr.ReadLine());
+                    }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            return lines;
+        }
+
+        
+        /// <summary>
+        /// Takes a list of a list of data.  Writes to file, using delimeter to seperate data.  Always overwrites.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="delimeter"></param>
+        /// <param name="rows"></param>
+        public void WriteFile(string filePath, char delimeter, List<List<string>> rows) {
+
+            using (TextWriter tw = new StreamWriter(filePath))
+            {
+                foreach (var row in rows)
+                {
+                    var csv = string.Join(delimeter.ToString(), row);
+                    tw.WriteLine(csv);
+       
+                }
+                tw.Close();
+            }
+            
+        }
+        
+        /// <summary>
+        /// Takes a list of strings and seperates based on delimeter.  Returns list of list of strings seperated by delimeter.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="delimeter"></param>
+        /// <returns></returns>
+        public List<List<string>> ParseData(List<string> data, char delimeter) {
+            List<List<string>> parsed = new List<List<string>>();
+
+            foreach (var row in data)
+            {
+                var newRow = new List<string>();
+                var split = row.Split(delimeter);
+                foreach (var item in split)
+                { 
+                    newRow.Add(item);
+                }
+                parsed.Add(newRow);
+            }
+
+            return parsed;
+
+        }
+        
+        /// <summary>
+        /// Takes a list of strings and seperates on comma.  Returns list of list of strings seperated by comma.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public List<List<string>> ParseCsv(List<string> data) {
+            List<List<string>> parsed = new List<List<string>>();
+
+            foreach (var row in data)
+            {
+                var newRow = new List<string>();
+                var split = row.Split(',');
+                foreach (var item in split)
+                {
+                    newRow.Add(item);
+                }
+                parsed.Add(newRow);
+            }
+
+            return parsed;
+        }
+    }
+}
